f1058848afdb6a730c91b1698df05c8f1b736943
Yure12 <mery112002@gmail.com>
2021-11-23 14:23:56 -0600

    Added sidebar for sorting

--- coppersmith updates.py
+++ coppersmith updates.py
@@ -13,14 +13,27 @@ credentials = service_account.Credentials.from_service_account_info(
 )
 conn =connect(credentials=credentials)
 
-@st.cache(ttl=600)
+@st.cache(ttl=300)
 def run_query(query):
     rows = conn.execute(query, headers=1)
     return rows
 
-sheet_url = st.secrets["public_gsheets_url"]
+sheet_url = st.secrets["private_gsheets_url"]
 rows = run_query(f'SELECT * FROM "{sheet_url}"')
+
+st.header("CopperSmith")
+
+st.subheader("Latest Updates")
 
 # Print results.
 for row in rows:
-    st.write(row)
+    st.write(row.Update)
+
+# Side bar
+st.sidebar.subheader("Sorting Options")
+
+date_options = st.sidebar.selectbox('Date', ('11/23/2021', '11/24/2021'))
+category_options = st.sidebar.selectbox('Category', ('Rivet Finish', 'Metal Size'))
+supplier_options = st.sidebar.selectbox('Supplier', ('Arcob', 'Artizen', 'Handcraft'))
+department_options = st.sidebar.selectbox('Department', ('Design', 'Sales', 'Production', 'Support'))
+
